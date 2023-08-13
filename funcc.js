32f249efd511627a2bf6c0fed83a59bd42657831
Ojal Patil <ojalp101@gmail.com>
2023-08-13 21:57:22 +0530

    Initial Commit'

--- funcc.js
+++
@@ -1,6 +0,0 @@
-const notificationIcon = document.querySelector('.notifications-icon-container');
-const notificationBar = document.querySelector('.notification-bar');
-
-notificationIcon.addEventListener('click', () => {
-    notificationBar.classList.toggle('show');
-});
